4cd18b966e770f923f4ed0bc08b53b52721b5198
Nirob <thenirobhasan@gmail.com>
2019-03-17 14:04:00 +0600

    add eprescription form

--- services/views.py
+++ services/views.py
@@ -2,6 +2,17 @@ from django.shortcuts import render
 from django.utils import timezone
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import TemplateView
+from services.models import Eprescription
+from services.forms import EprescriptionForm
 # Create your views here.
-class eprescription(TemplateView):
-    template_name = 'eprescription/eprescription.html'
+# class eprescription(TemplateView):
+#     template_name = 'eprescription/eprescription.html'
+def eprescription(request):
+    form = EprescriptionForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form = EprescriptionForm()
+    context = {
+        'form':form
+    }
+    return render(request,"eprescription/eprescription.html",context)
