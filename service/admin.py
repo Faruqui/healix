11fabd5babc328bfedb3affe94d6f1cd71c99dd2
Nirob <thenirobhasan@gmail.com>
2019-04-17 00:35:18 +0600

    add appointment

--- service/admin.py
+++ service/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from service.models import Prescription,Hospital
+from service.models import Prescription,Hospital,Location
 # Register your models here.
 admin.site.register(Prescription)
 admin.site.register(Hospital)
+admin.site.register(Location)
--- service/models.py
+++ service/models.py
@@ -14,14 +14,45 @@ class Prescription(models.Model):
     def get_absolute_url(self):
         return reverse('prescription-detail', kwargs={'pk': self.pk})
 
+class Location(models.Model):
+    city = models.CharField(max_length=50)
+    zip = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.address
+
+    class Admin:
+        list_display = ('city', 'country')
 
 class Hospital(models.Model):
     name = models.CharField(max_length=160)
-    area = models.CharField(null=True, max_length=200)
-    address = models.CharField(null=True, max_length=200)
-    phone = models.IntegerField(null=True)
+    phone = models.CharField(max_length=20)
+    location = models.OneToOneField(Location,on_delete=models.CASCADE,null = True)
 
 
     def __str__(self):
         return self.name
+    def __str__(self):
+        return self.name
+
+    class Admin:
+        list_display = (
+            'name',
+            'phone',
+            'location'
+        )
+
+class Appointment(models.Model):
+    doctor = models.ForeignKey(Doctor, related_name="doctors",on_delete=models.CASCADE)
+    patient = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="patients",on_delete=models.CASCADE)
+    description = models.CharField(max_length=200)
+    active = models.BooleanField(default=True)
+    hospital = models.ForeignKey(Hospital,on_delete=models.CASCADE)
+    startTime = models.TimeField()
+    endTime = models.TimeField()
+    date = models.DateField()
+
+    
