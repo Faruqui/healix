00a25a78bf9d2c5126a779178aa97106c84380c7
Nirob <thenirobhasan@gmail.com>
2019-04-18 22:40:19 +0600

    Home View Unit Test

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
 
 # Create your tests here.
+class MainAppTest(SimpleTestCase):
+    def test_home_view_exist(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code,200)
+
+    def test_home_about_exist(self):
+        response = self.client.get('/about/')
+        self.assertEqual(response.status_code,200)
